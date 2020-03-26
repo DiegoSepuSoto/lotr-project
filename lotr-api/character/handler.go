18a4eb2fcf6d10d2796e358f861baccf4c9314d8
Diego Sepúlveda <diegosepu.soto@gmail.com>
2020-03-26 14:01:10 -0300

    Change in mux router

--- lotr-api/character/handler.go
+++ lotr-api/character/handler.go
@@ -1,14 +1,11 @@
 package character
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
 // HandleRequests ...
-func HandleRequests() {
+func HandleRequests() *mux.Router {
 	rout := mux.NewRouter().StrictSlash(true)
 	rout.HandleFunc("/characters", allCharacters).Methods("GET")
 	rout.HandleFunc("/lotr", lotrCharacters).Methods("GET")
@@ -16,6 +13,5 @@ func HandleRequests() {
 	rout.HandleFunc("/silmarillion", silmCharacters).Methods("GET")
 	rout.HandleFunc("/up_vote/{id}", upVote).Methods("PUT")
 
-	f.Println("[Lord Of The Rings API] Running in port 8081")
-	log.Fatal(http.ListenAndServe(":8081", rout))
+	return rout
 }
--- lotr-api/main.go
+++ lotr-api/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	c "lotr-api/character"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	c.HandleRequests()
+	mainRout := mux.NewRouter()
+
+	cRout := c.HandleRequests()
+
+	mainRout.Handle("/", cRout)
+
+	fmt.Println("[Lord Of The Rings API] Running in port 8081")
+	log.Fatal(http.ListenAndServe(":8081", mainRout))
 }
